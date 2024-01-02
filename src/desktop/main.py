62733ac2ffd47b5ff2b47348143e8ec473df7911
Tarsis <br.tmvdl@gmail.com>
2024-01-02 13:48:18 -0300

    Updating app files

---
+++ src/desktop/main.py
@@ -0,0 +1,69 @@
+import flet
+
+def main(page: flet.Page):
+  page.title = "Foxbit WS"
+  print("Initial route:", page.route)
+
+  def get_title_app_bar(text):
+    return flet.AppBar(title=flet.Text(text))
+
+  def get_button(text, link):
+    return flet.ElevatedButton(text, on_click=lambda _: page.go(link))
+
+  def get_cancel_button():
+    return get_button("Cancel", "/")
+
+  def get_index_route():
+    return flet.View(
+      "/",
+      [
+        get_title_app_bar("Foxbit WS"),
+        get_button("List currencies", "/currencies"),
+        get_button("List markets", "/markets"),
+        get_button("List banks", "/banks"),
+        get_button("Get current time", "/system/time"),
+      ],
+    )
+
+  def get_currencies_route():
+    return flet.View("/currencies", [get_title_app_bar("List currencies"), get_cancel_button(),])
+
+  def get_markets_route():
+    return flet.View("/markets", [get_title_app_bar("List markets"), get_cancel_button(),])
+
+  def get_banks_route():
+    return flet.View("/banks", [get_title_app_bar("List banks"), get_cancel_button(),])
+
+  def get_system_time_route():
+    return flet.View("/system/time", [get_title_app_bar("Get current time"), get_cancel_button(),])
+
+  def get_route(route):
+    match route:
+      case "/currencies":
+        return get_currencies_route()
+      case "/markets":
+        return get_markets_route()
+      case "/banks":
+        return get_banks_route()
+      case "/system/time":
+        return get_system_time_route()
+
+  def route_change(route):
+    page.views.clear()
+    page.views.append(get_index_route())
+
+    if page.route != "/":
+      page.views.append(get_route(page.route))
+
+  page.update()
+
+  def view_pop(view):
+    page.views.pop()
+    top_view = page.views[-1]
+    page.go(top_view.route)
+
+  page.on_route_change = route_change
+  page.on_view_pop = view_pop
+  page.go(page.route)
+
+flet.app(target=main)
--- src/desktop/pack.sh
+++ src/desktop/pack.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/sh
 
-flet pack app.py
+flet pack main.py
 
 mkdir -p apps
 
-mv ./dist/app ./apps/app.$( date +%Y%m%d%H%M%S )
+mv ./dist/main ./apps/main.$( date +%Y%m%d%H%M%S )
