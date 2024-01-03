606f1ee123777c1f9658a562ed1f2f0872c078df
Tarsis <br.tmvdl@gmail.com>
2024-01-02 22:11:54 -0300

    Updating app files

--- src/desktop/main.py
+++ src/desktop/main.py
@@ -9,26 +9,48 @@ def main(page: flet.Page):
   def get_title_app_bar(text):
     return flet.AppBar(title=flet.Text(text))
 
-  def get_button(text, link):
-    return flet.ElevatedButton(text, on_click=lambda _: page.go(link))
+  def get_send_button(endpoint, fields):
+    def on_click(e):
+      print(endpoint)
+
+    return flet.OutlinedButton('send', on_click=on_click)
+
+  def get_save_button(endpoint, fields):
+    def on_click(e):
+      print(endpoint)
+
+    return flet.OutlinedButton('save', on_click=on_click)
+
+  def get_go_button(text, link):
+    def on_click(e):
+      page.go(link)
+
+    return flet.OutlinedButton(text, on_click=on_click)
 
   def get_cancel_button():
-    return get_button("Cancel", "/")
+    return get_go_button("Cancel", "/")
 
   def get_index_route():
     return flet.View(
       "/",
       [
         get_title_app_bar("Foxbit WS"),
-        get_button("Get current time", "/system/time"),
-        get_button("List currencies", "/currencies"),
-        get_button("List markets", "/markets"),
-        get_button("List banks", "/banks"),
+        get_go_button("Get current time", "/system/time"),
+        get_go_button("List currencies", "/currencies"),
+        get_go_button("List markets", "/markets"),
+        get_go_button("List banks", "/banks"),
       ],
     )
 
   def get_currencies_route():
-    return flet.View("/currencies", [get_title_app_bar("List currencies"), get_cancel_button(),])
+    endpoint = "/currencies"
+
+    title_bar = get_title_app_bar("List currencies")
+    send_button = get_send_button(endpoint, [])
+    save_button = get_save_button(endpoint, [])
+    cancel_button = get_cancel_button()
+
+    return flet.View(endpoint, [title_bar, send_button, save_button, cancel_button,])
 
   def get_markets_route():
     return flet.View("/markets", [get_title_app_bar("List markets"), get_cancel_button(),])
