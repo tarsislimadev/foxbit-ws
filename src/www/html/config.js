8c83a7dc85639345acc04de8e69acdde53b4413b
Tarsis <br.tmvdl@gmail.com>
2024-01-27 11:51:16 -0300

    Updating app files

--- src/www/html/config.js
+++ src/www/html/config.js
@@ -1,7 +1,7 @@
 // 
 
-export const user_id = 2723380349022200
+export const user_id = ''
 
-export const APIKey = 'xx4igx2SAKLV0Gxqq8RBphW5X4hAsslhEO8Sk89b'
+export const APIKey = ''
 
-export const secret_key = 'HabTDEFlravhn4f7q6TrRrXSUFWyTjd1MYvDHe5U'
+export const secret_key = ''
