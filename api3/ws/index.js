2ceec44bb5b0344b893236b17cc5410c9cb034c3
Tarsis <br.tmvdl@gmail.com>
2024-01-27 21:40:38 -0300

    Updating app files

--- api3/ws/index.js
+++ api3/ws/index.js
@@ -3,7 +3,6 @@ import express from 'express'
 import { createServer } from 'http'
 import { Server } from 'socket.io'
 import * as events from './events.js'
-import { switchRequest } from './foxbit.js'
 
 const app = express()
 const server = createServer(app)
@@ -12,26 +11,31 @@ const db = new Database({ type: 'fs', config: '/data' })
 
 const save = (table, data = {}) => db.in(table).new().writeMany(data)
 
-const createUrl = ({ Url }) => `https://api.foxbit.com.br/rest/v3/${Url}`
+const createUrl = (endpoint, input = {}) => `https://api.foxbit.com.br/rest/v3${input.Url}`
 
-const createHeaders = () => ({})
+const createMethod = (endpoint, input = {}) => 'GET'
 
-const createBody = ({ Body }) => Body
+const createHeaders = (endpoint, input = {}) => ({})
+
+const createBody = (endpoint, input = {}) => null
 
 io.on('connection', (socket) => {
   console.log('socket', socket.id)
 
-  const send = async (endpoint, input = {}) => {
+  const send = (endpoint, input = {}) => {
     console.log('send', endpoint, input)
     save(endpoint, input)
-    return await fetch({
-      url: createUrl(input),
-      headers: createHeaders(),
-      body: createBody(input)
-    }).then((res) => res.json())
+    const request = { method: createMethod(endpoint), headers: createHeaders(input), body: createBody(input) }
+    return fetch(createUrl(endpoint, input), request).then((res) => res.json())
   }
 
-  events.getEventsList().map((endpoint) => socket.on(endpoint, (input) => send(endpoint, input).then((output) => socket.emit(endpoint, output))))
+  const retrieve = (endpoint, output = {}) => {
+    console.log('retrieve', endpoint, output)
+    save(endpoint, output)
+    socket.emit(endpoint, output)
+  }
+
+  events.getEventsList().map((endpoint) => socket.on(endpoint, (input) => send(endpoint, input).then((output) => retrieve(endpoint, output))))
 })
 
 server.listen(80)
---
+++ currencies.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl -X GET -isSL 'https://api.foxbit.com.br/rest/v3/currencies'
