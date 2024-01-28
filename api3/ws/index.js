58ba2d717f5ab3f978710f6ddb81f88ddf649b0c
Tarsis <br.tmvdl@gmail.com>
2024-01-28 15:17:24 -0300

    Updating app files

--- api3/ws/index.js
+++ api3/ws/index.js
@@ -6,10 +6,38 @@ const app = express()
 const server = createServer(app)
 const io = new Server(server, { cors: { origin: '*' } })
 
+const save = (data) => data
+
+const json = (url, data = {}) => fetch(url, data).then((res) => res.json())
+
+const createUrlPath = (data) => `/rest/v3${data.body.Url}`
+
+const createUrl = (data) => `https://api.foxbit.com.br${createUrlPath(data)}`
+
+const createMethod = (data) => 'GET'
+
+const createHeaders = (data) => ({})
+
+const createbody = (data) => null
+
+const request = (data) => json(createUrl(data), { method: createMethod(data), headers: createHeaders(data), body: createbody(data) })
+
 io.on('connection', (socket) => {
   console.log('socket', socket.id)
-  setInterval(() => socket.emit('message', `server; ${Date.now()}`), 1000)
-  socket.on('message', (data) => console.log('socket message', data))
+
+  const send = (input) => {
+    console.log('send', input)
+    save(input)
+    return request(input)
+  }
+
+  const retrieve = (output) => {
+    console.log('retrieve', output)
+    save(output)
+    socket.emit('message', output)
+  }
+
+  socket.on('message', (input) => send(input).then((output) => retrieve({ input, output })))
 })
 
 server.listen(80)
--- api3/www/html/index.js
+++ api3/www/html/index.js
@@ -1,4 +1,6 @@
-import { HTML, nButton, nInputText } from '@brtmvdl/frontend'
+import { HTML, nH2, nFlex } from '@brtmvdl/frontend'
+import * as events from './utils/events.js'
+import * as tabs from './tabs/index.js'
 import 'socket.io'
 
 export class Page extends HTML {
@@ -7,21 +9,68 @@ export class Page extends HTML {
   }
 
   children = {
-    input: new nInputText(),
-    button: new nButton(),
-    events: new HTML(),
+    tabs: new HTML(),
   }
 
   onCreate() {
-    this.setEvents()
-    this.append(this.getInput())
-    this.append(this.getButton())
+    this.setHTMLEvents()
+    this.setSocketEvents()
+    this.append(this.getTitleH2())
+    this.append(this.getTabsHTML())
   }
 
-  setEvents() {
-    this.state.socket.on('connection', () => this.sendMessage(`client hello; ${Date.now()}`))
-    this.state.socket.on('message', (data) => this.onMessage(data))
-    setInterval(() => this.sendMessage(`client interval; ${Date.now()}`), 1000)
+  getTabHTML(tab) {
+    switch (tab) {
+      case 'List currencies': return new tabs.ListCurrenciesTab()
+      // case 'List markets': return new tabs.List marketsTab(),
+      // case 'Get a market quotation': return new tabs.Get a market quotationTab(),
+      // case 'Get order book': return new tabs.Get order bookTab(),
+      // case 'Get candles Deprecated': return new tabs.Get candles DeprecatedTab(),
+      // case 'Get candlesticks': return new tabs.Get candlesticksTab(),
+      // case 'List banks': return new tabs.List banksTab(),
+      // case 'Get current time': return new tabs.Get current timeTab(),
+      // case 'Get current member details': return new tabs.Get current member detailsTab(),
+      // case 'Create an order': return new tabs.Create an orderTab(),
+      // case 'List orders': return new tabs.List ordersTab(),
+      // case 'Get an order by SN Deprecated': return new tabs.Get an order by SN DeprecatedTab(),
+      // case 'Get an order by ID': return new tabs.Get an order by IDTab(),
+      // case 'Get an order by client ID': return new tabs.Get an order by client IDTab(),
+      // case 'Cancel orders': return new tabs.Cancel ordersTab(),
+      // case 'List trades': return new tabs.List tradesTab(),
+      // case 'Get member accounts': return new tabs.Get member accountsTab(),
+      // case 'Get member PnL data': return new tabs.Get member PnL dataTab(),
+      // case 'List deposits': return new tabs.List depositsTab(),
+      // case 'Get a deposit': return new tabs.Get a depositTab(),
+      // case 'Get a deposit address': return new tabs.Get a deposit addressTab(),
+      // case 'List withdrawals': return new tabs.List withdrawalsTab(),
+      // case 'Create a withdrawal': return new tabs.Create a withdrawalTab(),
+      // case 'Get a withdrawal': return new tabs.Get a withdrawalTab(),
+      // case 'List Transactional Limits': return new tabs.List Transactional LimitsTab(),
+    }
+
+    return new HTML()
+  }
+
+  onTab(tab) {
+    console.log('onTab', tab)
+    this.children.tabs.clear()
+    const html = this.getTabHTML(tab)
+    html.on('submit', ({ value }) => this.onSubmit(value))
+    this.on('message', ({ value }) => html.dispatchEvent('message', value))
+    this.children.tabs.append(html)
+  }
+
+  onSubmit(message) {
+    console.log('onSubmit', message)
+    this.sendMessage(message)
+  }
+
+  setHTMLEvents() {
+    this.on('tab', (ev) => this.onTab(ev.value))
+  }
+
+  setSocketEvents() {
+    this.state.socket.on('message', (data) => this.dispatchEvent('message', data))
   }
 
   sendMessage(message) {
@@ -29,23 +78,34 @@ export class Page extends HTML {
     this.state.socket.emit('message', message)
   }
 
-  onMessage(data) {
-    console.log('message', data)
+  getTitleH2() {
+    const h2 = new nH2()
+    h2.setText('Foxbit REST API 3.0')
+    return h2
   }
 
-  getInput() {
-    this.children.input.setPlaceholder('input')
-    return this.children.input
+  createTabHeader(header) {
+    const html = new HTML()
+    html.setText(header)
+    html.setStyle('cursor', 'pointer')
+    html.on('click', () => this.dispatchEvent('tab', header))
+    return html
   }
 
-  onButtonClick() {
-    const data = this.children.input.getValue()
-    this.sendMessage(data)
+  getTabsHeaders() {
+    const html = new HTML()
+    events.getEventsList().map((tab) => html.append(this.createTabHeader(tab)))
+    return html
   }
 
-  getButton() {
-    this.children.button.setText('request')
-    this.children.button.on('click', () => this.onButtonClick())
-    return this.children.button
+  getTabsContents() {
+    return this.children.tabs
+  }
+
+  getTabsHTML() {
+    const tabs = new nFlex()
+    tabs.append(this.getTabsHeaders().setContainerStyle('width', '20%'))
+    tabs.append(this.getTabsContents().setContainerStyle('width', '80%'))
+    return tabs
   }
 }
